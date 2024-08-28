f78d34d1d3beaa03b046fe614fa1368baa1feebf
Samuel L P Duque <samuel.locatel@souunit.com.br>
2024-08-28 11:47:56 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,66 @@
+menu = """
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+def depositar(valor):
+    global saldo, extrato
+    if valor > 0:
+        saldo += valor
+        extrato += f"Depósito: R$ {valor:.2f}\n"
+    else:
+        print("Operação falhou! O valor informado é inválido.")
+
+def sacar(valor):
+    global saldo, extrato, numero_saques
+    if valor <= 0:
+        print("Operação falhou! O valor informado é inválido.")
+        return
+
+    if valor > saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+    elif valor > limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+    elif numero_saques >= LIMITE_SAQUES:
+        print("Operação falhou! Número máximo de saques excedido.")
+    else:
+        saldo -= valor
+        extrato += f"Saque: R$ {valor:.2f}\n"
+        numero_saques += 1
+
+def exibir_extrato():
+    print("\n================ EXTRATO ================")
+    if not extrato:
+        print("Não foram realizadas movimentações.")
+    else:
+        print(extrato)
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("==========================================")
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do depósito: "))
+        depositar(valor)
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+        sacar(valor)
+
+    elif opcao == "e":
+        exibir_extrato()
+
+    elif opcao == "q":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
